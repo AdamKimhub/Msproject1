81b7a0fdf49ba81b9e372e4f1f27d46a60d081c1
AdamKimhub <adamk6522@gmail.com>
2025-05-09 18:12:34 +0300

    added features and taackled the time problem

--- to_graph.py
+++ to_graph.py
@@ -7,26 +7,31 @@ from pymatgen.core import Structure, PeriodicSite, DummySpecie
 from pymatgen.analysis.local_env import MinimumDistanceNN
 
 def struct_to_dict(structure):
-    list_of_sites = structure.sites
-    list_of_frac_coords = np.round(structure.frac_coords,3)
-    structure_dict = {i: j for i, j in zip(list_of_sites, list_of_frac_coords)}
-    return structure_dict
+    rounded_coords = np.round(structure.frac_coords, 3)
+    return {tuple(coord): site for coord, site in zip(rounded_coords, structure.sites)}
 
-def get_index(ref_struct, a_site):
-    for index, site in enumerate(ref_struct.sites):
-        if np.array_equal(site.coords, a_site.coords):
-            return index
+def fe_site(original, new):
+    # from elements.csv
+    element_pot = element_pot = {"Mo":-10.9332, "S":-4.127, "W":-13.0106, "Se":-3.489,
+                   "B":-6.704, "N":-8.324, "Ga":-3.03, "In":-2.715,
+                   "P":-5.362, "V":-8.992, "O":-4.938, "C":-9.226}
+    
+    if new == 0: # For vcancy
+        fe_defect = element_pot[original]* -1
 
+    else: # For substitution
+        fe_defect = (element_pot[original]* -1) + element_pot[new]
+        
+    return fe_defect
 
 def get_defects_structure(defective_struct, reference_struct):
-    copy_defective_struct = defective_struct.copy()
     mindnn = MinimumDistanceNN()
     # struct to dict
-    defective_dict = struct_to_dict(copy_defective_struct)
+    defective_dict = struct_to_dict(defective_struct)
     reference_dict = struct_to_dict(reference_struct)
 
     # Get lattice of defective structure
-    structure_lattice = copy_defective_struct.lattice
+    structure_lattice = defective_struct.lattice
 
     # List to add all defect sites
     defects_list = []
@@ -34,29 +39,32 @@ def get_defects_structure(defective_struct, reference_struct):
     # Dictionary to hold properties of each defect site
     defects_properties = {} 
 
-    for ref_site, ref_coords in reference_dict.items():
-        matching = False
-        for def_site, def_coords in defective_dict.items():
-            if np.array_equal(ref_coords, def_coords):
-                matching = True
-                if ref_site.specie != def_site.specie: # Substitution case
-                    # Add site to defects list
-                    defects_list.append(def_site)
+    for ref_coord, ref_site in reference_dict.items():
+        # Use the reference coordinates to get the defective site
+        def_site = defective_dict.get(ref_coord)
 
-                    # Get atomic number change and defect type
-                    add_property = {"original_an":ref_site.specie.Z,
-                                    "new_an": def_site.specie.Z,
-                                    "an_change": def_site.specie.Z - ref_site.specie.Z,
-                                    "vacancy_defect": 0.0,
-                                    "substitution_defect": 1.0,
-                                    "bonds_broken": 0.0}
-                    defects_properties[def_site] = add_property
+        if def_site:  # The site is found in both the reference structure and the defective structure
+            # But are the species the same?
+            if ref_site.specie != def_site.specie:  # Substitution
+                # Add site to defects list
+                defects_list.append(def_site)
 
-        if not matching: # Vacancy case
+                # Get atomic number change and defect type
+                add_property = {"original_an":ref_site.specie.Z,
+                                "new_an": def_site.specie.Z,
+                                "an_change": def_site.specie.Z - ref_site.specie.Z,
+                                "vacancy_defect": 0.0,
+                                "substitution_defect": 1.0,
+                                "bonds_broken": 0.0,
+                                "site_fe": fe_site(ref_site.species_string,def_site.species_string)}
+                defects_properties[def_site] = add_property
+
+        else: # the site from ref_structure is not found in defective structure
+            # This means that the site is a vacancy site
             # Add site to defective structure
             vacant_site = PeriodicSite(
                 species= DummySpecie(),
-                coords= ref_coords,
+                coords= ref_coord,
                 coords_are_cartesian= False, 
                 lattice= structure_lattice
                 )
@@ -70,7 +78,8 @@ def get_defects_structure(defective_struct, reference_struct):
                           "an_change": 0 - ref_site.specie.Z,
                           "vacancy_defect": 1.0,
                           "substitution_defect": 0.0,
-                          "bonds_broken": mindnn.get_cn(reference_struct, get_index(reference_struct, ref_site))}
+                          "bonds_broken": mindnn.get_cn(reference_struct, reference_struct.sites.index(ref_site)),
+                          "site_fe": fe_site(ref_site.species_string,0)}
             defects_properties[vacant_site] = add_property
 
     # create a defects structure
@@ -91,25 +100,38 @@ def get_nodes_edges(structure):
     # The nodes: These are the sites features
     nodes = []
     for i, site in enumerate(sites_list):
-        node_features = [i, site.properties["bonds_broken"], site.properties["original_an"], site.properties["new_an"],
-                         site.properties["an_change"], site.properties["vacancy_defect"],
-                         site.properties["substitution_defect"]]
+        node_features = [i, site.properties["bonds_broken"], site.properties["original_an"], 
+                         site.properties["new_an"], site.properties["an_change"], 
+                         site.properties["vacancy_defect"], site.properties["substitution_defect"], 
+                         site.properties["site_fe"]]
         # Node features syntax
-        '''[index of site, number of nearset neighbors to site, Z_before defect,
-        Z_after defect, Z_change, is site vac_site(1  for yes, 0 for no), is site sub_site(1 for yes, 0 for no)]'''
         nodes.append(node_features)
          
 
     # The edges
     edges = [] # The sites in relation
     edge_features = [] # The distance between each site
+
+    from_e = []
+    to_e = []
     
     for i, site_i in enumerate(sites_list):
         for j, site_j  in enumerate(sites_list):
-            if i != j:
-                edges.append([i,j])
-                dist = site_i.distance(site_j)
-                edge_features.append([dist])
+            from_e.append(i)
+            to_e.append(j)
+            # Get distance between sites
+            dist = site_i.distance(site_j)
 
+            # Are the defects the same or different
+            if site_i.properties["an_change"] == site_j.properties["an_change"]:
+                same_diff = 1
+            else:
+                same_diff = 0
+
+            # What is the site_fe difference
+            site_fe_diff = np.abs(site_i.properties["site_fe"] - site_j.properties["site_fe"])
+
+            edge_features.append([dist,same_diff,site_fe_diff])
+    edges.append(from_e)
+    edges.append(to_e)
     return nodes, edges, edge_features
-
